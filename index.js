0673c7d2e196a67441c3e59d3c1d202572009844
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-05-05 22:01:09 +0600

    cookie not showing issue solved

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const morgan = require("morgan");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ezfvwv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
@@ -27,6 +35,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const carCollection = client.db("carDoctorDB").collection("services");
+    const bookingCollection = client.db("carDoctorDB").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = carCollection.find();
@@ -35,22 +44,57 @@ async function run() {
     });
 
     app.post("/bookings", async (req, res) => {
-      const bookings = req.body;
-      console.log(bookings);
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // create token
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      res.send({ success: true });
+    });
+
+    app.get("/bookings", async (req, res) => {
+      console.log("tookeeen: ", req.cookies.token);
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
     });
 
     app.get("/services/:id", async (req, res) => {
-      console.log(2);
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
       const result = await carCollection.findOne(query, options);
       // console.log(result);
       res.send(result);
     });
+    // ====get some data====
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
